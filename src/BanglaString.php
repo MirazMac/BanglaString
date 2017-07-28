307b93f36bd3f7745008e9995bdbaeac4a30db72
Miraz Mac <mirazmac@gmail.com>
2017-07-28 19:12:58 +0600

    Initial Commit

---
+++ src/BanglaString.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace MirazMac\BanglaString;
+
+/**
+* BanglaString
+*
+* A wannabe all-in-all Bangla String Manupulation Library!
+*
+* @author Miraz Mac <mirazmac@gmail.com>
+* @link https://mirazmac.info Author Homepage
+* @version 0.1
+* @license LICENSE The MIT License
+* @package MirazMac\BanglaString
+*/
+
+class BanglaString
+{
+    /**
+     * Loaded string
+     *
+     * @var string
+     */
+    protected $string;
+
+    /**
+     * Create a new BanglaString instance
+     *
+     * @param string $string The text to be converted
+     * @throws \InvalidArgumentException When passed parameter is not string
+     */
+    public function __construct($string)
+    {
+        if (!is_string($string)) {
+            throw new \InvalidArgumentException(__CLASS__ .'::translate() expects parameter $string to be string, ' . gettype($string) . ' given!');
+        }
+
+        $this->string = $string;
+    }
+
+    /**
+     * This is identical as self::__construct(), but the difference is it allows to call the method  statically
+     *
+     * @param string $string The text to be converted
+     * @return object
+     */
+    public static function translate($string)
+    {
+        return new self($string);
+    }
+
+    /**
+     * Translate the loaded string to Bijoy ANSI
+     *
+     * @return string
+     */
+    public function toBijoy()
+    {
+        return Translator\AvroUnicode::getInstance()->translate($this->string);
+    }
+
+    /**
+     * Translate the loaded string to Avro Unicode
+     *
+     * @return string
+     */
+    public function toAvro()
+    {
+        return Translator\BijoyAnsi::getInstance()->translate($this->string);
+    }
+}
---
+++ src/Translator/BijoyAnsi.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace MirazMac\BanglaString\Translator;
+
+/**
+* Translates Bengali text written in Bijoy ANSI to Avro Unicode
+*
+* @version 0.1
+* @since 0.1
+* @package MirazMac\BanglaString
+*/
+
+class BijoyAnsi
+{
+    /**
+     * Static instance of the class
+     *
+     * @var object
+     */
+    public static $instance = null;
+
+
+    private function __construct()
+    {
+    }
+
+    private function __wakeup()
+    {
+    }
+
+    private function __clone()
+    {
+    }
+
+    /**
+     * Get an instance of the class
+     *
+     * @return object
+     */
+    public static function getInstance()
+    {
+        if (!self::$instance) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    /**
+     * Translates Bijoy ANSI text to Avro
+     *
+     * @param  string $string Text written via bijoy
+     * @return string
+     */
+    public function translate($string)
+    {
+        // Import character maps
+        $charmap = CharacterMap::getLetterCharMap();
+        $kars = CharacterMap::getAvroKars();
+
+        // Pre-replacement - All the letters, numbers and juktabornas..
+        $string = str_replace(array_values($charmap), array_keys($charmap), $string);
+
+        // Build the regex pattern for post replacement
+        $regex = "/{$kars}/um";
+        // Post-replacement - All kars
+        $string = preg_replace_callback($regex, [$this, 'translateKarsCallback'],
+                        $string);
+        // Finally hand-over the string!
+        return $string;
+    }
+
+    /**
+     * Callback method for self::translate()
+     *
+     * @param  array  $match The matches
+     * @return string
+     */
+    protected function translateKarsCallback($match = [])
+    {
+        switch ($match[1]) {
+            case 'w':
+                return $match[2] . 'ি'; // E~ kar
+
+            case '†':
+                return $match[2] . 'ে'; // A~ kar
+
+            case '‡':
+                return $match[2] . 'ে'; // A~ Kar ( Bijoy has 2 of 'em )
+        }
+    }
+}
