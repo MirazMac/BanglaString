6de15430413434b54f172d205837578511570d42
Miraz Mac <mirazmac@gmail.com>
2017-07-28 19:13:25 +0600

    Added composer.json and example.php

---
+++ example.php
@@ -0,0 +1,26 @@
+<?php
+require 'vendor/autoload.php';
+
+use MirazMac\BanglaString\BanglaString;
+
+echo BanglaString::translate('wgqvu n„`‡qi PÂjZv e‡Ü eªZx n‡j Rxeb cwic‚Y© n‡e bvbv i‡Oi dy‡j| KyÅwUKv cÖfÄb k¼vi KviY jÊfÊ K‡i hvq aivi A½b| w¶ß n‡j mv½ n‡e weÁR‡b e‡j kvš— n‡j G eªþv‡Ê evwÃZdj †g‡j| Avlv‡p Ckvb †Kv‡b nVvr So D‡V MMb †g‡N‡Z Xv‡K e„wó bv‡g gv‡V Elvi AvKv‡k bv‡g mÜ¨vi Qvqv H †`‡Lv †_‡g †M‡Q cvivcv‡i †Lqv| kir FZy‡Z Pvu` Av‡jvq Askygvb myL `ytL cvkv cvwk mn Ae¯’vb| †h R‡j‡Z Ck¦i Z„òv †gUvq An english is fine? †mB R‡j‡Z RxeKy‡j webvk NUvq| †ivM hw` †`n †Q‡o g‡b wM‡q a‡i Jl‡ai mva¨ wK ev Zv‡i my¯’ K‡i ? Hello Dear!')->toAvro();
+?>
+<hr>
+<?php
+echo BanglaString::translate('হৃদয়ের চঞ্চলতা বন্ধে ব্রতী হলে
+জীবন পরিপূর্ণ হবে নানা রঙের ফুলে।
+কুঞ্ঝটিকা প্রভঞ্জন শঙ্কার কারণ
+লণ্ডভণ্ড করে যায় ধরার অঙ্গন।
+ক্ষিপ্ত হলে সাঙ্গ হবে বিজ্ঞজনে বলে
+শান্ত হলে এ ব্রহ্মাণ্ডে বাঞ্ছিতফল মেলে।
+আষাঢ়ে ঈশান কোনে হঠাৎ ঝড় উঠে
+গগন মেঘেতে ঢাকে বৃষ্টি নামে মাঠে
+ঊষার আকাশে নামে সন্ধ্যার ছায়া
+ঐ দেখো থেমে গেছে পারাপারে খেয়া।
+শরৎ ঋতুতে চাঁদ আলোয় অংশুমান
+সুখ দুঃখ পাশা পাশি সহ অবস্থান।
+যে জলেতে ঈশ্বর তৃষ্ণা মেটায় An english is fine?
+সেই জলেতে জীবকুলে বিনাশ ঘটায়।
+রোগ যদি দেহ ছেড়ে মনে গিয়ে ধরে
+ঔষধের সাধ্য কি বা তারে সুস্থ করে!')->toBijoy();
+?>
